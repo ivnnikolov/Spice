1bdf695d80bcadde500ad6aed0006c2ad3b601a2
Ivan Nikolov <ivan.nikolov.yeah@gmail.com>
2020-12-16 11:54:41 -0600

    Add project files.

---
+++ Spice/Data/Migrations/20201221153705_ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Spice.Data.Migrations
+{
+    public partial class ShoppingCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "ShoppingCart",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "ShoppingCart");
+        }
+    }
+}
---
+++ Spice/Data/Migrations/20201224215635_OrderDetails.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Spice.Data.Migrations
+{
+    public partial class OrderDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "OrderDetails",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "OrderDetails");
+        }
+    }
+}
